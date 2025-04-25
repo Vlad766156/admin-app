a1eab7164e0bf6848fde64a875b0f813b0493575
Vlad <vladkochetov591@gmail.com>
2025-04-25 18:18:52 +0200

    proverka na admina

--- admin app.cpp
+++ admin app.cpp
@@ -10,21 +10,14 @@ int main()
     cout << "are you admin?\n";
     cin >> admin;
     bool is_admin = admin == "yes";
-    if (years >= 18 and is_admin) {
-        cout << "you can pass\n";
-    }
-    else {
-        cout << "access denied\n";
-        if (years < 18) {
-            cout << "you are not adult\n";
-        }
+    (years >= 18 && is_admin) ? cout << "you can pass\n" : (
+        cout << "access denied\n"
+        years < 18 ? cout << "you are not adult\n" : cout << ""
+        admin == "no" ? cout << "you not admin\n" : (
+            admin != "yes" ? cout << "invalid input try again\n" : cout << "")
+        );
 
-        if (admin == "no") {
-            cout << "you not admin\n";
-        }
-        else if (admin != "yes") {
-            cout << "invalid input try again\n";
-        }
-    }
+    return 0;
+    
 }
 
